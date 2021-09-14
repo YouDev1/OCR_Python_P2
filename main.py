f96a29909cfd7ed7a2f0f776ba7cbdc9c54a4fb5
Youssef <youssef0806@hotmail.fr>
2021-09-14 13:53:07 +0200

    écriture de l'en-tête et de la fonction 'inf' pour éctire les informations

--- main.py
+++ main.py
@@ -2,3 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
+# liste des en-têtes
+en_tetes = ['product_page_url', "upc", "title", "price_including_tax", "price_excluding_tax", "number_available", "product_description", "category", "review_rating", "image_url"]
+
+# écriture de l'en-tête
+
+with open("TEST.csv", "w", encoding=("utf-8")) as f:
+    writer = csv.writer(f, delimiter=',')
+    writer.writerow(en_tetes)
+
+
+# écriture des informations
+def inf(infos):     
+    with open("TEST.csv", 'a', encoding=("utf-8")) as f:
+        writer = csv.writer(f, delimiter=',')
+        writer.writerow(infos)
